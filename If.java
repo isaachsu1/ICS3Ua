975513436861b6eba8e89923fa4f8f8133ae8a20
isaachsu1 <114429838+isaachsu1@users.noreply.github.com>
2022-09-29 00:12:33 -0400

    Add files via upload

---
+++ If.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author isaac
+ */
+public class If {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+    int i = 10;
+
+  if (i > 15)
+   System.out.println("10 is less than 15");
+
+  // This statement will be executed
+  // as if considers one statement by default
+  System.out.println("I am Not in if");
+ }
+}

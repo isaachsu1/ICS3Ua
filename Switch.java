374d644bc31bf53a71fbae44a8155eb96f833ec8
isaachsu1 <114429838+isaachsu1@users.noreply.github.com>
2022-10-04 01:26:03 -0400

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author isaac
+ */
+public class Switch {
+
---
+++ scanner_class_practice.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author isaac
+ */
+import java.util.Scanner;
+public class scanner_class_practice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+    
+
+    Scanner input = new Scanner(System.in);
+    
+    Scanner input1 = new Scanner(System.in);
+    
+    
+
+    System.out.println("enter the values:");
+    
+    int num= input.nextInt();
+    int num1= input1.nextInt();
+    int sum=num+num1;
+    
+    
+    System.out.println("value is");
+    System.out.println(sum);
+    
+    
+  
+        
+        
+    }
+}
